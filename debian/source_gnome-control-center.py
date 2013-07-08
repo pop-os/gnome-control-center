ce8c54e1b811a213b96bf381e633b3a19d974618
Iain Lane <iain.lane@canonical.com>
2013-07-08 18:05:39 +0100

    * debian/compat: bump to 9 * debian/*.install: fix install paths for arched /usr/lib * debian/source_gnome-control-center.py: fix apport hook for new libdirs * debian/patches/zz_add_fallback_panels_dir.patch: add a patch to look in   the old panel dir for compatibility with existing packages * debian/rules: set the correct --libdir (work around apparent cdbs issue)
    (bzr r574)

--- debian/source_gnome-control-center.py
+++ debian/source_gnome-control-center.py
@@ -1,11 +1,12 @@
 import os, apport.packaging, re
+import glob
 from apport.hookutils import *
 
 def add_info(report):
 	# the issue is not in the gnome-control-center code so reassign
-	if "Stacktrace" in report and "/usr/lib/control-center-1" in report["Stacktrace"]:
+	if "Stacktrace" in report and "/control-center-1/" in report["Stacktrace"]:
 		for words in report["Stacktrace"].split():
-			if words.startswith("/usr/lib/control-center-1"):
+			if words.startswith("/usr/lib/") and "/control-center-1/" in words:
 			    if apport.packaging.get_file_package(words) != 'gnome-control-center':
     				report.add_package_info(apport.packaging.get_file_package(words))
     				return    				
@@ -16,14 +17,14 @@ def add_info(report):
 
 	# collect informations on the /usr/lib/control-center-1 components 
 	plugin_packages = set()
-	for dirpath, dirnames, filenames in os.walk("/usr/lib/control-center-1"):
-		for filename in filenames:
-			path = os.path.join(dirpath, filename)
-			package = apport.packaging.get_file_package(path)
-			if package == 'gnome-control-center':
-				continue
+	for paneldir in (['/usr/lib/control-center-1'] + glob.glob('/usr/lib/*/control-center-1')):
+		for dirpath, dirnames, filenames in os.walk(paneldir):
+			for filename in filenames:
+				path = os.path.join(dirpath, filename)
+				package = apport.packaging.get_file_package(path)
+				if package == 'gnome-control-center':
+					continue
 
-			plugin_packages.add(package)
+				plugin_packages.add(package)
 
-	report["usr_lib_gnome-control-center"] = package_versions(*sorted(plugin_packages))
-
+		report["usr_lib_gnome-control-center"] = package_versions(*sorted(plugin_packages))
